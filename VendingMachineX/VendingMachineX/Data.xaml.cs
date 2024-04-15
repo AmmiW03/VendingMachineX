bf01466d7988ba6172280251b033109799325d71
CristopherBustosMejia <cristopher_1321124038@uptecamac.edu>
2024-04-15 13:38:33 -0600

    Agregar archivos de proyecto.

---
+++ VendingMachineX/VendingMachineX/Data.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VendingMachineX
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Data : TabbedPage
+    {
+        public Data()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VendingMachineX/VendingMachineX/MachineDetails.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VendingMachineX
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MachineDetails : ContentPage
+    {
+        public MachineDetails()
+        {
+            InitializeComponent();
+        }
+
+        private async void Products(object sender, EventArgs e)
+        {
+            // Aquí es donde se realiza la navegación a la otra página
+            await Navigation.PushAsync(new Products());
+        }
+    }
+}
